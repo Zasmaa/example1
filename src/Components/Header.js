9ebcb22530be55d1f5636e41e2e35f4ed4b01c83
Zasmaa <39232461+Zasmaa@users.noreply.github.com>
2018-11-12 01:23:40 -0600

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+
+class Header extends React.Component{
+
+
+	 render() {
+    return (
+<div className="Header">
+<h1>Neighbooh App</h1>
+
+</div>
+    	)
+  }
+
+}
+
+export default Header;
---
+++ src/Components/Map.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+
+class Map extends React.Component{
+
+
+
+
+
+	 render() {
+    return (
+
+       <main>
+        <div id="map"></div>
+     </main>
+      
+    );
+  }
+
+}
+
+export default Map;
